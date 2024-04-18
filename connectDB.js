8d267c99c721f2376d88f3a4c821f079acb96f03
Saksham Vasudev <97773012+Saksham1710@users.noreply.github.com>
2024-04-18 14:32:42 -0700

    Update connectDB.js

--- connectDB.js
+++ connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 
-    const url = "mongodb://localhost:27017/ArtList";
+    const url = "mongodb+srv://saksham:Saksham%40123@cluster0.j5qv4li.mongodb.net/ArtList";
     try {
         const connectionInstance = await mongoose.connect(url);
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
